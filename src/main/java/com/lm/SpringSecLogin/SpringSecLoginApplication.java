346563f3da4b680a1c1f94cd6dfde55ec90dd692
Luca <corradoluca2000@gmail.com>
2024-06-24 16:29:04 +0200

    Background structure

---
+++ src/main/java/com/lm/SpringSecLogin/SpringSecLoginApplication.java
@@ -0,0 +1,13 @@
+package com.lm.SpringSecLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecLoginApplication.class, args);
+	}
+
+}
