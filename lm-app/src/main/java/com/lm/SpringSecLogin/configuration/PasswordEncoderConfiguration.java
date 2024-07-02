49f8524beaa61b09ab01cd333bf3fc6f526173a4
Marco Sette <msette999@gmail.com>
2024-07-02 16:17:39 +0200

    .

--- lm-app/src/main/java/com/lm/SpringSecLogin/configuration/PasswordEncoderConfiguration.java
+++ lm-app/src/main/java/com/lm/SpringSecLogin/configuration/PasswordEncoderConfiguration.java
@@ -1,6 +1,6 @@
 package com.lm.SpringSecLogin.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;                         // Importa la classe Bean
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
